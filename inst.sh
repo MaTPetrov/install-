b26b2af0d79314828c61edb31b18a043afe050be
MaTPetrov <99609580+MaTPetrov@users.noreply.github.com>
2025-05-18 15:55:43 +0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -5,7 +5,7 @@ sudo pacman -S discord
 sudo pacman -S telegram-desktop
 sudo pacman -S steam
 sudo pacman -S xournalpp
-sudo pacman -S pycharm-community-edition
+#sudo pacman -S pycharm-community-edition
 
 
 
@@ -14,7 +14,7 @@ sudo pacman -S flatpak
 flatpak install flathub dev.qwery.AddWater
 flatpak install flathub org.libreoffice.LibreOffice
 #flatpak install flathub ru.linux_gaming.PortProton
-flatpak install flathub ru.yandex.Browser
+#flatpak install flathub ru.yandex.Browser
 flatpak install flathub com.github.PintaProject.Pinta
 flatpak install flathub io.github.flattool.Warehouse
 
